a35f82aa014ff98ff733c638582572e0c3c8f063
Ampherost <Ampherost235@gmail.com>
2024-09-09 14:39:54 -0700

    Added functions and variables for board

--- src/board.h
+++ src/board.h
@@ -2,10 +2,20 @@
 #define BOARD_H
 
 class Board {
+public:
+    Board();
 
+    void print() const;
+
+    bool isCellEmpty(int index) const;
+
+    void setCell(int index, char player);
+
+    char getCell(int index) const;
+
+private:
+char board[9];
 
 };
-
-
 
 #endif  
