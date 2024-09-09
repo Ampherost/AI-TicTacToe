edd102a12c5dd72f4b68d1ea617ca044c2af0738
Ampherost <Ampherost235@gmail.com>
2024-09-09 16:52:10 -0700

    Added helper functions in utility file

--- src/utility.h
+++ src/utility.h
@@ -1,6 +1,15 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include "board.h"
 
+// Checks if a player has won the game
+bool checkWin(const Board& board, char player);
+
+// Checks if the game is a draw
+bool checkDraw(const Board& board);
+
+// Gets input from the player and update the board
+void playerMove(Board& board, char player);
 
 #endif  
